424e894a5d0ed9216e6970eb2661e7e0a4dbc79c
briankipchirchir <kbrianngetich@gmail.com>
2026-03-19 15:40:08 +0300

    kk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiGatewayUrl: 'https://smart-commerce-nw5g.onrender.com',
   productServiceUrl: 'https://smart-commerce-product-service.onrender.com',
-  orderServiceUrl:'http://localhost:8083'
+  orderServiceUrl:'https://smart-commerce-order-service.onrender.com'
 };
